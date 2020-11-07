b1662ebeef6f99955c9f016b3ebaac4ac6e456db
mahdy151 <mahdy151saadat@gmail.com>
2020-11-07 22:04:58 +0330

    test

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 var tl = gsap.timeline({paused:true}); 
 var textWrapper = document.querySelector('.product-name');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// var res = str.split(" ");
 var letters = $('.letter');
 
 
-tl.from(letters, {duration:1 , opacity:0 , scale:1 , y:10 , rotation:10 ,  ease:"power3.inOut", stagger: 0.025}, "+=0.2");
+tl.from(letters, {duration:1 , opacity:0 , scale:1 , y:20 , rotation:10 ,  ease:"power3.inOut", stagger: 0.025}, "+=0.2");
 
 $(".card-container").mouseenter(function() {
 tl.restart();
