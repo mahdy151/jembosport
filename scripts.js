a787e220ecb122ee4be2c07fce6a0086283339a4
mahdy151 <mahdy151saadat@gmail.com>
2020-11-08 23:02:26 +0330

    test

--- scripts.js
+++ scripts.js
@@ -1,98 +1,129 @@
 var textWrapper = document.querySelector('.product-name');
-var results = Splitting({ target: textWrapper, by: 'words' });
+var results = Splitting({
+    target: textWrapper,
+    by: 'words'
+});
 var words = document.querySelectorAll(".word");
-var card = document.querySelector('.product-img');
-card.addEventListener("mouseenter", function() {
-    // anime.remove('.word');
-    var animation = anime({
-        targets: '.word',
-        // delay: 500,
-        top:0,
-        // translateY: [0,"1.1em"],
-        // translateX: "0.55em",
-        // translateZ: 0,
-        rotateZ: 0,
-        opacity: 1,
-        // duration: 1000,
-        easing: "easeOutExpo",
-        delay: anime.stagger(100, {start: 200})
-    });
+var card = document.querySelector('.card-container');
 
-    document.querySelector(".product-info-box").style.opacity = "1";
-    document.querySelector(".product-badge").style.opacity = "1";
-    document.querySelector(".product-price").style.top = "0px";
-
-});
-
-card.addEventListener("mouseout", function() {
-    anime.remove('.word');
-    var animation = anime({
-        targets: '.word',
-        top:30,
-        rotateZ: 10,
-        opacity: 0,
-        // duration: 5000,
-        // delay: 500,
-        easing: "easeOutBack",
-        delay: anime.stagger(200, {start: 200 , direction: 'reverse'})
-
-
-
-
-    
-    
-    });
-    document.querySelector(".product-info-box").style.opacity = "0";
-    document.querySelector(".product-badge").style.opacity = "0";
-    document.querySelector(".product-price").style.top = "50px";
-
-
-});
-// tl.from(letters, {duration:1 , opacity:0 , scale:1 , y:20 , rotation:10 ,  ease:"power3.inout", stagger: 0.3}, "+=0.2");
-
- 
 // const animation = anime({
 //     targets: '.word',
-//     translateY: ["1.1em", 0],
-//     translateX: ["0.55em", 0],
-//     translateZ: 0,
-//     rotateZ: [10, 0],
-//     duration: 1000,
+//     top:0,
+//     rotateZ: 0,
+//     opacity: 1,
 //     autoplay: false,
-//     easing: "easeOutExpo",
-//     offset: '-=4000',
-//     delay: (el, i) => 300 * i
+//     easing: "easeOutQuad",
+//     delay: anime.stagger(200, {start: 200})
 // });
-// animation.reverse()
 
-// $(".card-container").mouseenter(function() {
+// const infobox = anime({
+//     targets: '.product-info-box',
+//     opacity: [0,1],
+//     autoplay: false,
+//     easing: "easeOutQuad",
+// });
+
+// function animateIn() {
+//     anime.remove('.product-info-box' , '.word');
+//     document.querySelector(".product-badge").style.opacity = "1";
+//     document.querySelector(".product-price").style.top = "0px";
+//     infobox.play();
 //     animation.play();
-//  });
+// };
 
-// $(".card-container").mouseleave(function() {
-//     animation.restart();
+// function animateOut() {
+//     anime.remove('.product-info-box' , '.word');
+//     document.querySelector(".product-badge").style.opacity = "0";
+//     document.querySelector(".product-price").style.top = "50px";
+//     infobox.reverse();
+//     animation.reverse();
+// };
+
+// card.onmouseenter = function() {animateIn()};
+// card.onmouseleave = function() {animateOut()};
+
+
+var In = anime.timeline({
+    autoplay: false,
+});
+
+In
+.add({
+    targets: '.product-info-box',
+    autoplay: false,
+    opacity: 1,
+    duration: 300,
+    easing: "easeOutQuad",
+})
+.add({
+    targets: '.word',
+    autoplay: false,
+    top: 0,
+    rotateZ: 0,
+    opacity: 1,
+    easing: "easeOutQuad",
+    duration:750,
+    delay: anime.stagger(100, {
+        start: 10
+    }),
+});
+
+card.onmouseenter = function() {
+    In.restart();
+    // In.remove('.word');
+    // In.remove('.product-info-box');
+};
+
+var Out = anime.timeline({
+    autoplay: false,
+});
+
+Out
+.add({
+    targets: '.word',
+    autoplay: false,
+    top: 40,
+    rotateZ: 20,
+    opacity: 0,
+    easing: "easeOutQuad",
+    duration:500,
+    delay: anime.stagger(100, {start: 10 , direction: 'reverse'}),
+    })
+.add({
+    targets: '.product-info-box',
+    autoplay: false,
+    opacity: 0,
+    duration: 500,
+    easing: "easeOutQuad",
+});
+
+card.onmouseleave = function() {
+    Out.restart();
+    // Out.remove('.word');
+    // Out.remove('.product-info-box');
+};
+
+
+// card.addEventListener("mouseleave", function() {
+//     anime.remove('.product-info-box');
+//     anime.remove('.word');
+//     var animation = anime({
+//         targets: '.word',
+//         top:40,
+//         rotateZ: 20,
+//         opacity: 0,
+//         easing: "easeOutQuad",
+//         delay: anime.stagger(200, {start: 100 , direction: 'reverse'})
+//     });
+
+//     var infobox = anime({
+//         targets: '.product-info-box',
+//         opacity: 0,
+//         easing: "easeOutQuad",
+//         delay: 500,
+//     });
+//     document.querySelector(".product-badge").style.opacity = "0";
+//     document.querySelector(".product-price").style.top = "50px";
+
+
 // });
-
-
-
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.word',
-//     translateY: ["1.1em", 0],
-//     translateX: ["0.55em", 0],
-//     translateZ: 0,
-//     rotateZ: [10, 0],
-//     duration: 750,
-//     easing: "easeOutExpo",
-//     delay: (el, i) => 300 * i
-//   }).add({
-//     targets: '.word',
-//     translateY: [0,"1.1em"],
-//     translateX: [0 ,"0.55em"],
-//     translateZ: 0,
-//     rotateZ: [0, 10],
-//     easing: "easeInExpo",
-//     duration: 1200,
-//     delay: (el, i) => 200 + 30 * i
-//   });
