2e0b4c74aa724595b4c8e8b4a81cb0a32baeeba8
mahdy151 <mahdy151saadat@gmail.com>
2020-11-25 13:09:12 +0330

    svg-path-fixed

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ In
             return i * 500
         },
         begin: function (anim) {
-            $('.loop1').attr("stroke", "black")
+            $('.loop1').attr("stroke", "white")
             $('.loop1').attr("fill", "transparent")
             $('svg').removeClass('completed')
             $('.loop2').removeClass('completedpath')
@@ -28,7 +28,7 @@ In
         },
         offset: '-=1500',
         begin: function (anim) {
-            $('.loop2').attr("stroke", "white")
+            $('.loop2').attr("stroke", "rgb(30, 30, 30)")
             $('.loop2').attr("fill", "transparent")
         },
         complete: function (anim) {
